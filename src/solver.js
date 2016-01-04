8dfdc4c29704eff96ee69226acc43cf0f48263ed
Gaétan Hasbroucq <g.hasbroucq@gmail.com>
2016-01-04 20:02:54 +0100

    Vérification des carrés

--- src/solver.js
+++ src/solver.js
@@ -13,43 +13,96 @@
 
         var lines = data.split(os.EOL);
         console.log(lines);
-        console.log('line' + lines[0][0]);
 
         lines.forEach(function (line, li) {
             lines[li] = line.split('');
         });
 
+	lines = iterate(lines);
+console.log(emptyCells(lines));
+	lines = iterate(lines);
+console.log(emptyCells(lines));
+	lines = iterate(lines);
+console.log(emptyCells(lines));
+	lines = iterate(lines);
+console.log(emptyCells(lines));
+	lines = iterate(lines);
+console.log(emptyCells(lines));
+	lines = iterate(lines);
+console.log(emptyCells(lines));
+	lines = iterate(lines);
+console.log(emptyCells(lines));
+	lines = iterate(lines);
+console.log(emptyCells(lines));
+
+
+        lines.forEach(function (line, li) {
+            lines[li] = line.join('');
+        });
+
+	console.log(lines);
+        console.log("End...");
+    });
+
+
+function iterate(lines){
         lines.forEach(function (line, li) {
             line.forEach(function (cell, ci) {
-                // Initialisation
+// Initialisation
+  //              console.log('===== ' + li + ' - ' + ci);
+
                 if (cell == ' '){
                     var possibles = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-                    for (var i=0; i< 10; i++){
-
+// Lignes
+                    for (var i=0; i< 9; i++){
                         var index = possibles.indexOf(parseInt(line[i]));
                         if (index > -1) {
                             possibles.splice(index, 1);
                         }
                     }
+//                    console.log('L : ' + possibles);
 
-                    for (var i=0; i< 10; i++){
-
-                        var index = possibles.indexOf(parseInt(lines[ci][i]));
+// Colonnes
+                    for (var i=0; i< 9; i++){
+                        var index = possibles.indexOf(parseInt(lines[i][ci]));
                         if (index > -1) {
                             possibles.splice(index, 1);
                         }
                     }
+//                    console.log('C : ' + possibles);
 
-                    console.log(li + ' - ' + ci + ' : ' + possibles);
+// Carrés
+//		    console.log(Math.floor(li/3) + " - " + Math.floor(ci/3));
+		    for (var i=Math.floor(li/3)*3; i < Math.floor(li/3) + 3; i++){
+                        for (var j=Math.floor(ci/3)*3; j < Math.floor(ci/3) + 3; j++){
+                            var index = possibles.indexOf(parseInt(lines[i][j]));
+                            if (index > -1) {
+                                possibles.splice(index, 1);
+                            }
+                    	}
+                    }
+
+//                    console.log('X : ' + possibles);
+
+		    if (possibles.length == 0) throw 'Erreur';
+
+		    if (possibles.length == 1) {
+			lines[li][ci] = possibles[0];
+		    }
 
                 }
 
             });
         });
+return lines;
+}
 
-        console.log("End...");
-    });
-
+function emptyCells(lines){
+	var count = 0;
+        lines.forEach(function (line, li) {
+            count = count + line.filter(function(item){return item == ' '}).length;
+        });
+	return count;
+}
 
 })();
